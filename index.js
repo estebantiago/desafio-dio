05a2729595d82883852b4f56d7038dea51f4a919
estebantiago <estebantiago22@gmail.com>
2024-04-27 16:57:53 -0300

    eviando para o git

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-console.log('teste');
+
+
+let nomeDoHeroi = 'Rei Arthur';
+let quantidadeDeXp = 10003;
+
+
+
+if (quantidadeDeXp < 1000) {
+    console.log(' O herói de nome ' + nomeDoHeroi + ' está no nível de Ferro' );
+
+} else if (quantidadeDeXp >= 1001 && quantidadeDeXp < 2000) {
+    console.log(' O herói de nome ' + nomeDoHeroi + ' está no nível de Bronze' );
+
+} else if (quantidadeDeXp >= 2001 && quantidadeDeXp < 5000) {
+    console.log(' O herói de nome ' + nomeDoHeroi + ' está no nível de Prata' );;
+
+} else if (quantidadeDeXp >= 5001 && quantidadeDeXp < 6000){
+    console.log(' O herói de nome ' + nomeDoHeroi + ' está no nível de Ouro' );
+
+} else if (quantidadeDeXp >= 6001 && quantidadeDeXp < 7000){
+    console.log(' O herói de nome ' + nomeDoHeroi + ' está no nível de Platina' );
+
+} else if (quantidadeDeXp >= 7001 && quantidadeDeXp < 8000) {
+    console.log(' O herói de nome ' + nomeDoHeroi + ' está no nível de Ascendente' );
+
+} else if (quantidadeDeXp >= 8001 && quantidadeDeXp < 9000){
+        console.log(' O herói de nome ' + nomeDoHeroi + ' está no nível de Imortal' );
+        
+} else { 
+    console.log(' O herói de nome ' + nomeDoHeroi + ' está no nível de Radiante' );
+}
+
+
 
 
